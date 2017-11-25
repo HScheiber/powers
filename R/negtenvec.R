b2a94446311888789855cd2d0213ad23b97a07a8
Hayden Scheiber <scheiber@chem.ubc.ca>
2017-11-24 16:31:21 -0800

    Fixed bug.

--- R/negtenvec.R
+++ R/negtenvec.R
@@ -4,4 +4,4 @@
 #'
 #' @format This (and the next) is a new tag that applies for data documentation.
 #' @source Note that you shouldn't use the `@export` tag when documenting data!
-"tenvec"
+"negtenvec"
